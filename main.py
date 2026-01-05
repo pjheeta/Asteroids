5d7aa22a96f05a6808fc27130c017cf854d2142c
pjheeta <pjheeta.gmail.com>
2026-01-04 20:30:22 -0500

    Created For Loop to keep Screen active

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 import pygame
+from logger import log_state
+
 
 def main():
     print("Starting Asteroids with pygame version: VERSION!")
+    print (f"Screen width: {SCREEN_WIDTH}")
+    print (f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+
+    while True:
+    # Your code here
+        log_state() 
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+        return
+        screen.fill((0, 0, 0))  # Fill the screen with black
+        pygame.display.flip() 
 
 if __name__ == "__main__":
     main()
